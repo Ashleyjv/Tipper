e489660a3d01ae6c0080e1240ed1ef88bb76e356
Ashleyjv <54588325+Ashleyjv@users.noreply.github.com>
2022-01-14 16:36:41 -0500

    Second Commit

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -23,6 +23,14 @@ class ViewController: UIViewController {
     
     
     @IBAction func calculateTip(_ sender: Any) {
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        let tipPercentages = [0.15, 0.18, 0.20]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+        
     }
     
 }
